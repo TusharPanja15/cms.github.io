ba330698b977e0c93fb0bc9feed2a999b0b39241
TusharPanja15 <78247623+TusharPanja15@users.noreply.github.com>
2021-03-08 21:37:44 +0530

    update

---
+++ js/common.js
@@ -0,0 +1,23 @@
+// NAVBARS
+// For index.html
+$(document).ready(function () {
+    $('#navs').load("common/navbar.html");
+});
+
+// For news.html
+$(document).ready(function () {
+    $('#navs').load("../common/navbar.html");
+});
+
+//-----------------------------------------------------------------------------------------------------------------------------
+
+// FOOTER
+// For index.html
+$(document).ready(function () {
+    $('#footer').load("common/footer.html");
+});
+
+// For news.html
+$(document).ready(function () {
+    $('#footer').load("../common/footer.html");
+});
